08969e4ef391b6c1afe351fa4653d2b8d05c23e8
Martin Ohlson <martin@moid.se>
2019-04-27 18:36:11 +0200

    Prettier UI, WIP3

--- src/components/Bar.vue
+++ src/components/Bar.vue
@@ -43,20 +43,35 @@ $widthMultipliers: (
   align-items: center;
   justify-content: center;
   color: #fff;
-  font-size: 8px;
+  font-size: 0.1rem;
   border-width: 1px;
   border-style: solid;
+
+  // Small devices (landscape phones, 576px and up)
+  @media (min-width: 576px) {
+    font-size: 0.6rem;
+  }
+
+  // Medium devices (tablets, 768px and up)
+  @media (min-width: 768px) {
+    font-size: 0.8rem;
+  }
+
+  // Large devices (desktops, 992px and up)
+  @media (min-width: 992px) {
+    font-size: 1rem;
+  }
 }
 
 @each $name, $value in $heightMultipliers {
   .bar-#{$name} {
-    height: 0rem + ($value * 3) - 0.5rem;
+    height: 0em + ($value * 3) - 0.5em;
   }
 }
 
 @each $name, $value in $widthMultipliers {
   .bar-#{$name} {
-    width: 0rem + ($value) + 0.2rem;
+    width: 0em + ($value * 0.7);
   }
 }
 </style>
